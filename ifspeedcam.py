9309b87cdafeead25b3d8b968a0c1be7d0a7d783
josephenewman <josephnewmanyt@gmail.com>
2023-01-20 14:50:46 -0600

    if statements for speed, etc
    if statements for speed, checks if speed is decreasing, activates camera

---
+++ ifspeedcam.py
@@ -0,0 +1,28 @@
+import time
+import datetime
+from picamera import PiCamera
+
+def check_speed(speed):
+    if speed < 8 and speed_decreasing(speed):
+        print("Safe to walk.")
+    elif 8 <= speed < 25:
+        print("Wait.")
+    elif speed >= 25 and not speed_decreasing(speed):
+        print("STOP.")
+        activate_speedcamera()
+
+def speed_decreasing(speed):
+    # code to check if speed is decreasing goes here
+    pass
+
+def activate_speedcamera():
+
+    camera = PiCamera()
+    time.sleep(2)
+
+    while True:
+        file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".h264"
+        camera.start_recording(file_name)
+        camera.wait_recording(10)
+        camera.stop_recording()
+        print("Done")
